0f55357266bdf125888abc5b82473b3e7298a725
matheusmagnani <mrmatheusmagnani@gmail.com>
2025-01-20 15:52:56 -0300

    test: now it is possible to do e2e user registration testing

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,5 +1,34 @@
-import { test } from "vitest";
+import request from "supertest"
+import { app } from "src/app"
+import { afterAll, beforeAll, describe, expect, it } from "vitest"
 
-test('ok', () => {
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to register', async () => {
+    await request(app.server)
+      .post('/users')
+      .send({
+        name: 'Matheus Magnani',
+        email: 'matheus@gmail.com',
+        password: '123456'
+      })
+
+    const response = await request(app.server)
+    .post('/session')
+    .send({
+      email: 'matheus@gmail.com',
+      password: '123456'
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String)
+    })
+  })
 })
