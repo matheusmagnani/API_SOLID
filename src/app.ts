50c53685a3c61ca4defcb4ef11cdb28d9ac93d29
matheusmagnani <mrmatheusmagnani@gmail.com>
2025-01-20 16:24:07 -0300

    test: now we have a controller for creating gyms

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
-import { appRoutes } from "./http/routes";
+import { usersRoutes } from "./http/controllers/users/routes";
+import { gymsRoutes } from "./http/controllers/gyms/routes";
 import { ZodError } from "zod";
 import { env } from "./env";
 import "@fastify/jwt";
@@ -10,7 +11,9 @@ app.register(require('@fastify/jwt'), {
   secret: env.JWT_SECRET
 })
 
-app.register(appRoutes)
+app.register(usersRoutes)
+app.register(gymsRoutes)
+
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
---
+++ src/http/controllers/gyms/create.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+import { FastifyRequest, FastifyReply } from "fastify"
+import { makeCreateGymUseCase } from "src/use-cases/factories/make-create-gym-use-case";
+
+export async function create(request: FastifyRequest, reply: FastifyReply){
+  const createGymsBodySchema = z.object({
+    title: z.string(),
+    description: z.string().nullable(),
+    phone: z.string().nullable(),
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    })
+  })
+
+  const { title, description, phone, latitude, longitude } = createGymsBodySchema.parse(request.body);
+
+
+  const createGymUseCase = makeCreateGymUseCase()
+
+  await createGymUseCase.execute({
+      title,
+      description,
+      phone,
+      latitude,
+      longitude
+    })
+
+   
+
+  return reply.status(201).send();
+  }
