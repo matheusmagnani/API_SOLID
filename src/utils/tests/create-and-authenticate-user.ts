b80b5010f00df727b0435ed88dc9c361954d53af
matheusmagnani <mrmatheusmagnani@gmail.com>
2025-01-21 17:23:21 -0300

    fix: now we no longer have errors in e2e tests

--- src/utils/tests/create-and-authenticate-user.ts
+++ src/utils/tests/create-and-authenticate-user.ts
@@ -1,15 +1,18 @@
+import { hash } from "bcryptjs"
 import { FastifyInstance } from "fastify"
+import { prisma } from "src/lib/prisma"
 import request from "supertest"
 
-export async function createAndAuthenticateUser(app: FastifyInstance){
-  await request(app.server)
-        .post('/users')
-        .send({
+export async function createAndAuthenticateUser(app: FastifyInstance, isAdmin = false){
+      await prisma.user.create({
+        data: {
           name: 'Matheus Magnani',
           email: 'matheus@gmail.com',
-          password: '123456'
-        })
-  
+          password_hash: await hash('123456', 6),
+          role: isAdmin ? 'ADMIN' : 'MEMBER'
+        }
+      })
+
       const authResponse = await request(app.server)
         .post('/session')
         .send({
